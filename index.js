daf603af2f80b4d584374221fcac4ccb93022b81
App004 <tora68818@gmail.com>
2024-10-25 11:00:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 function open-1(){
   window.open("./menu")
 }
+function copy() {
+  var copyText = document.getElementById("myInput");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  alert("Copied the text: " + copyText.value);
+}
